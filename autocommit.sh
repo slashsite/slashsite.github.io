08d7581b7cb20ed2eb1ba1e26e0a73235e3c0734
someone <superwibr@gmail.com>
2020-06-15 10:52:00 -0400

    First commit

--- autocommit.sh
+++ autocommit.sh
@@ -1,7 +1,7 @@
 cd desktop/slashsite.github.io
-git remote set-url origin git@github.com:slashsite/slashsite.github.io.git
+git remote set-url origin https://github.com/slashsite/slashsite.github.io.git
 git init
 git add .
 git commit -m "First commit"
-git remote add origin git@github.com:slashsite/slashsite.github.io.git
+git remote add origin https://github.com/slashsite/slashsite.github.io.git
 git push -u origin master
