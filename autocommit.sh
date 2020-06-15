c2bcaa52ca5c202bab0039a77680403fd0e66390
someone <superwibr@gmail.com>
2020-06-15 11:02:20 -0400

    Updated Master

--- autocommit.sh
+++ autocommit.sh
@@ -1,6 +1,12 @@
 cd desktop/slashsite.github.io
+
 git init
+
 git add .
+
 git commit -m "Updated Master"
+echo ^^^^^^^^^^^^^^^^^^^
+
 git remote set-url origin git@github.com:slashsite/slashsite.github.io.git
+
 git push -u origin master
